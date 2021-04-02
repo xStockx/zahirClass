e1dfca782fb18d2b25f52215c9544266ea054842
Javier Rain S <javier@rains.cl>
2021-04-02 20:16:35 -0300

    primero

---
+++ variables.js
@@ -0,0 +1,19 @@
+const PI = 3.1416;
+
+var nombreVariable = 'valor';
+var resultado = 4;
+
+pesoZahir = 91;
+pesoVecino = "91";
+//75 > 80 == false
+if(pesoZahir > pesoVecino)
+    {
+       console.log("Zahir pesa mas que el vecino") 
+    }
+else if(pesoZahir ===  pesoVecino){
+    console.log("Pesan lo mismos el par de watones")
+}
+
+else{
+        console.log("El Vecino es mas pesado que zahir")
+}
